121a5ee102114af15c3f1f74d97ad9a33609a619
Pierre <pierre.liaubet2@gmail.com>
2018-06-17 18:41:34 +0200

    Encore plus fini qu'avant

--- app/src/main/java/fr/pierreliaubet/au_clock/Home.java
+++ app/src/main/java/fr/pierreliaubet/au_clock/Home.java
@@ -24,6 +24,8 @@ public class Home extends AppCompatActivity
 
     Utilisateur lePigeon;
 
+    static Home home;
+
     List<News> lesNews = new ArrayList<>();
 
     int affiche;
@@ -38,13 +40,13 @@ public class Home extends AppCompatActivity
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        home = this;
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(Home.home, ListeExercices.class));
             }
         });
 
